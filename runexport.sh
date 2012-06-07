06923c1986226b60409741fefa53b9a8a589ca46
James Michael DuPont <mdupont@(none).(none)>
2012-06-07 10:38:48 +0000

    udpate

--- runexport.sh
+++ runexport.sh
@@ -1,2 +1,3 @@
 export PYTHONPATH=/home/mdupont/experiments/wikipedia/wikiteamgit/
+cd /home/mdupont/experiments/wikipedia/wikiteamgit/data
 python /home/mdupont/experiments/wikipedia/wikiteamgit/dumpgenerator.py   --api=http://en.wikipedia.org/w/api.php --xml
