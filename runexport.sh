33df2ce2b56431c7bad3fa26c8c49849a35ab284
Violetta Pollara DuPont <violetta@space-station.(none)>
2012-06-16 09:09:47 -0400

    merging

--- runexport.sh
+++ runexport.sh
@@ -1,7 +1,4 @@
 export PYTHONPATH=$HOME/experiments/wikiteam/
 cd ${HOME}/experiments/wikiteam/data
-python $HOME/experiments/wikiteam/dumpgenerator.py   --api=http://en.wikipedia.org/w/api.php --xml
-
-# now upload the wikia
-cd $HOME/experiments/pywikipediabot/
-python process.py
+python $HOME/experiments/wikiteam/dumpgenerator.py
+python $HOME/experiments/wikiteam/process.py
